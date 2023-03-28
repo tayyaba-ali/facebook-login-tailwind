25ce7536ace14c6e97dafe4558446b6229b0d754
Tayyaba Ali <98578632+tayyaba-ali@users.noreply.github.com>
2023-03-28 07:29:31 +0500

    completed the mobile first approach

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['*'],
+	theme: {
+		extend: {
+			backgroundColor: {
+				greenCustom: '#42b72a',
+			},
+		},
+	},
+	plugins: [],
+};
