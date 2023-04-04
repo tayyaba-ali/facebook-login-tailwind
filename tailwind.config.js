7888513f9570cf74d80374e43f5dc7d2e448249a
Tayyaba Ali <98578632+tayyaba-ali@users.noreply.github.com>
2023-04-04 21:49:11 +0500

    making my breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ module.exports = {
 			backgroundColor: {
 				greenCustom: '#42b72a',
 			},
+			screens: {
+				sm: '640px',
+				'my-breakpoint': '441px', // new breakpoint
+				md: '768px',
+				lg: '1024px',
+				xl: '1280px',
+				'2xl': '1536px',
+			},
 		},
 	},
 	plugins: [],
